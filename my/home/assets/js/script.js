dbdbf130c12ddfad5dc87ca2896701d2e8bd1d8e
SHAIK NAGUL MEERA <104340617+NAGULME@users.noreply.github.com>
2024-08-07 14:09:58 +0530

    Add files via upload

---
+++ my/home/assets/js/script.js
@@ -0,0 +1,136 @@
+'use strict';
+
+// modal variables
+const modal = document.querySelector('[data-modal]');
+const modalCloseBtn = document.querySelector('[data-modal-close]');
+const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
+
+// modal function
+const modalCloseFunc = function () { modal.classList.add('closed') }
+
+// modal eventListener
+modalCloseOverlay.addEventListener('click', modalCloseFunc);
+modalCloseBtn.addEventListener('click', modalCloseFunc);
+
+
+
+
+
+// notification toast variables
+const notificationToast = document.querySelector('[data-toast]');
+const toastCloseBtn = document.querySelector('[data-toast-close]');
+
+// notification toast eventListener
+toastCloseBtn.addEventListener('click', function () {
+  notificationToast.classList.add('closed');
+});
+
+
+
+
+
+// mobile menu variables
+const mobileMenuOpenBtn = document.querySelectorAll('[data-mobile-menu-open-btn]');
+const mobileMenu = document.querySelectorAll('[data-mobile-menu]');
+const mobileMenuCloseBtn = document.querySelectorAll('[data-mobile-menu-close-btn]');
+const overlay = document.querySelector('[data-overlay]');
+
+for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
+
+  // mobile menu function
+  const mobileMenuCloseFunc = function () {
+    mobileMenu[i].classList.remove('active');
+    overlay.classList.remove('active');
+  }
+
+  mobileMenuOpenBtn[i].addEventListener('click', function () {
+    mobileMenu[i].classList.add('active');
+    overlay.classList.add('active');
+  });
+
+  mobileMenuCloseBtn[i].addEventListener('click', mobileMenuCloseFunc);
+  overlay.addEventListener('click', mobileMenuCloseFunc);
+
+}
+
+
+
+
+
+// accordion variables
+const accordionBtn = document.querySelectorAll('[data-accordion-btn]');
+const accordion = document.querySelectorAll('[data-accordion]');
+
+for (let i = 0; i < accordionBtn.length; i++) {
+
+  accordionBtn[i].addEventListener('click', function () {
+
+    const clickedBtn = this.nextElementSibling.classList.contains('active');
+
+    for (let i = 0; i < accordion.length; i++) {
+
+      if (clickedBtn) break;
+
+      if (accordion[i].classList.contains('active')) {
+
+        accordion[i].classList.remove('active');
+        accordionBtn[i].classList.remove('active');
+
+      }
+
+    }
+
+    this.nextElementSibling.classList.toggle('active');
+    this.classList.toggle('active');
+
+  });
+
+
+}
+function loaderAnimation() {
+  var loader = document.querySelector("#loader")
+  setTimeout(function () {
+      loader.style.top = "-100%"
+  }, 4200)
+}
+loaderAnimation()
+
+
+
+/**
+ * CUSTOM CURSOR
+ */
+
+const cursor = document.querySelector("[data-cursor]");
+const anchorElements = document.querySelectorAll("a");
+const buttons = document.querySelectorAll("button");
+
+// change cursorElement position based on cursor move
+document.body.addEventListener("mousemove", function (event) {
+  setTimeout(function () {
+    cursor.style.top = `${event.clientY}px`;
+    cursor.style.left = `${event.clientX}px`;
+  }, 100);
+});
+
+// add cursor hoverd class
+const hoverActive = function () { cursor.classList.add("hovered"); }
+
+// remove cursor hovered class
+const hoverDeactive = function () { cursor.classList.remove("hovered"); }
+
+// add hover effect on cursor, when hover on any button or hyperlink
+addEventOnElements(anchorElements, "mouseover", hoverActive);
+addEventOnElements(anchorElements, "mouseout", hoverDeactive);
+addEventOnElements(buttons, "mouseover", hoverActive);
+addEventOnElements(buttons, "mouseout", hoverDeactive);
+
+// add disabled class on cursorElement, when mouse out of body
+document.body.addEventListener("mouseout", function () {
+  cursor.classList.add("disabled");
+});
+
+// remove diabled class on cursorElement, when mouse in the body
+document.body.addEventListener("mouseover", function () {
+  cursor.classList.remove("disabled");
+});
